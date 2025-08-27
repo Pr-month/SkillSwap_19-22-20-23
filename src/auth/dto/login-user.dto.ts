68b12cab92445f5979747617f756c9d267bd1312
unknown <ishimbay-avon@yandex.ru>
2025-08-27 16:24:38 +0500

    Последние задачи #2

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,9 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginUserDto {
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'Электронная почта пользователя',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'password123',
+    description: 'Пароль пользователя (минимум 6 символов)',
+    minLength: 6,
+  })
   @IsString()
   @MinLength(6)
   password: string;
--- src/auth/dto/refresh-token.dto.ts
+++ src/auth/dto/refresh-token.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class RefreshTokenDto {
+  @ApiProperty({ example: 'sample_refresh_token' })
   @IsString()
   refreshToken: string;
 }
